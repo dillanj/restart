da90a7f143c95915953b4601504462078ce70503
Dillan Johnson <dillanjohnson@Dillans-MacBook-Pro.local>
2022-09-17 18:11:49 -0600

    lesson 21 finished

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -8,102 +8,53 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    // MARK: - PROPERTY
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
-    
-    // MARK: - BODY
-    
-    
-    
-    var body: some View {
-        ZStack {
-            Color("ColorBlue")
-                .ignoresSafeArea(.all, edges: .all)
-            VStack(spacing: 20) {
-                // MARK: - HEADER
-                Spacer()
-                VStack(spacing: 0){
-                    Text("Share.")
-                        .font(.system(size: 60))
-                        .fontWeight(.heavy)
-                        .foregroundColor(.white)
-                    Text("""
-                    It's not how much we give, but how much love we put into giving
-                    """)
-                    .font(.title3)
-                    .fontWeight(.light)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 10)
-                } //: HEADER
-                // MARK: - BODY
-              ZStack {
-                  ZStack {
-                      Circle()
-                          .stroke(.white.opacity(0.2), lineWidth: 40)
-                          .frame(width: 260,  height: 260, alignment: .center)
-                      Circle()
-                          .stroke(.white.opacity(0.2), lineWidth: 80)
-                          .frame(width: 260,  height: 260, alignment: .center)
-                  } //: ZSTACK
-                  Image("character-1")
-                  .resizable()
-                  .scaledToFit()
-              }
-              Spacer()
-                // MARK: - FOOTER
-              
-              ZStack {
-                // 1. background
-                Capsule()
-                  .fill(Color.white.opacity(0.2))
-                
-                Capsule()
-                  .fill(Color.white.opacity(0.2))
-                  .padding(8)
-                // 2. call to action
-                Text("Get Started")
-                  .font(.system(.title3, design: .rounded))
-                  .fontWeight(.bold)
-                  .foregroundColor(.white)
-                  .offset(x: 20)
-                // 3. capsule
-                HStack {
-                  Capsule()
-                    .fill(Color("ColorRed"))
-                    .frame(width: 80)
-                  
-                  Spacer()
-                }
-                // 4. circle (draggable)
-                HStack {
-                  ZStack {
-                    Circle()
-                      .fill(Color("ColorRed"))
-                    Circle()
-                      .fill(.black.opacity(0.15))
-                      .padding(8)
-                    Image(systemName: "chevron.right.2")
-                      .font(.system(size: 24, weight: .bold))
-                  } //: Z STACK
-                  .foregroundColor(.white)
-                  .frame(width: 80, height: 80, alignment: .center)
-                  .onTapGesture {
-                    isOnboardingViewActive = false
-                  }
-                  Spacer()
-                } //: HSTACK
-              } // : FOOTER
-              .frame(height: 80, alignment: .center)
-              .padding()
-            } //: VSTACK
-        } // : ZSTACK
-    }
+  
+  // MARK: - PROPERTY
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
+  
+  var body: some View {
+    VStack(spacing: 20) {
+      // MARK: - HEADER
+      Spacer()
+      
+      ZStack {
+        CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+        Image("character-2")
+          .resizable()
+          .scaledToFit()
+        .padding()
+      }
+      
+      // MARK: - BODY
+      Text("The time that leads to mastery is dependent on the mintesnity of our focus.")
+        .font(.title3)
+        .fontWeight(.light)
+        .foregroundColor(.secondary)
+        .multilineTextAlignment(.center)
+        .padding()
+      
+      // MARK: - FOOTER
+      Spacer()
+      Button(action: {
+        isOnboardingViewActive = true
+      }) {
+        Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+          .imageScale(.large)
+        
+        Text("Restart")
+          .font(.system(.title3, design: .rounded))
+          .fontWeight(.bold)
+      } //: BUTTON
+      .buttonStyle(.borderedProminent)
+      .buttonBorderShape(.capsule)
+      .controlSize(.large)
+      
+    } //: VSTACK
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView()
+  }
 }
--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -8,31 +8,99 @@
 import SwiftUI
 
 struct OnboardingView: View {
-    // MARK: - PROPERTY
-    // The below line will access that key/value if it exists, and create it if not
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-    
-    
-    // MARK: - BODY
-    
-    
-    
-    var body: some View {
-        VStack(spacing: 20){
-            Text("Onboarding")
-                .font(.largeTitle)
+  // MARK: - PROPERTY
+  // The below line will access that key/value if it exists, and create it if not
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  
+  
+  // MARK: - BODY
+  
+  
+  
+  var body: some View {
+    ZStack {
+      Color("ColorBlue")
+        .ignoresSafeArea(.all, edges: .all)
+      VStack(spacing: 20) {
+        // MARK: - HEADER
+        Spacer()
+        VStack(spacing: 0){
+          Text("Share.")
+            .font(.system(size: 60))
+            .fontWeight(.heavy)
+            .foregroundColor(.white)
+          Text("""
+                  It's not how much we give, but how much love we put into giving
+                  """)
+          .font(.title3)
+          .fontWeight(.light)
+          .foregroundColor(.white)
+          .multilineTextAlignment(.center)
+          .padding(.horizontal, 10)
+        } //: HEADER
+        
+        // MARK: - BODY
+        ZStack {
+          
+          CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+          
+          Image("character-1")
+            .resizable()
+            .scaledToFit()
+        }
+        Spacer()
+        // MARK: - FOOTER
+        
+        ZStack {
+          // 1. background
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+          
+          Capsule()
+            .fill(Color.white.opacity(0.2))
+            .padding(8)
+          // 2. call to action
+          Text("Get Started")
+            .font(.system(.title3, design: .rounded))
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            .offset(x: 20)
+          // 3. capsule
+          HStack {
+            Capsule()
+              .fill(Color("ColorRed"))
+              .frame(width: 80)
             
-            Button(action: {
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
+            Spacer()
+          }
+          // 4. circle (draggable)
+          HStack {
+            ZStack {
+              Circle()
+                .fill(Color("ColorRed"))
+              Circle()
+                .fill(.black.opacity(0.15))
+                .padding(8)
+              Image(systemName: "chevron.right.2")
+                .font(.system(size: 24, weight: .bold))
+            } //: Z STACK
+            .foregroundColor(.white)
+            .frame(width: 80, height: 80, alignment: .center)
+            .onTapGesture {
+              isOnboardingViewActive = false
             }
-        } // : VSTACK
-    }
+            Spacer()
+          } //: HSTACK
+        } // : FOOTER
+        .frame(height: 80, alignment: .center)
+        .padding()
+      } //: VSTACK
+    } // : ZSTACK
+  }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
-    static var previews: some View {
-        OnboardingView()
-    }
+  static var previews: some View {
+    OnboardingView()
+  }
 }
